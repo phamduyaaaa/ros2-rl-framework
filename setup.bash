01b00bc18971695bbde5cadb42d957ca3d4786d3
Pham Duc Duy <134459693+phamduyaaaa@users.noreply.github.com>
2025-05-08 23:00:32 +0700

    setup.bash

---
+++ setup.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/phamduyaaaa/rl_training.git
+git clone https://github.com/phamduyaaaa/action_interfaces.git
+git clone https://github.com/phamduyaaaa/env_info.git
